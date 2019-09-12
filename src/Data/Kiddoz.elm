c0bc962f4cd19e4420cf95ed0d87e7b82ce7958a
Rémy HUBSCHER <remy@chefclub.tv>
2019-09-12 17:25:07 +0200

    Add ingredients, conversion table and a recipe model.

--- src/Data/Kiddoz.elm
+++ src/Data/Kiddoz.elm
@@ -1,23 +1,261 @@
-module Data.Kiddoz exposing (Ingredient, Lists, emptyLists)
+module Data.Kiddoz exposing
+    ( Ingredient
+    , Kind(..)
+    , Recipe
+    , Section
+    , Step
+    , Unit(..)
+    , emptyIngredient
+    , emptyRecipe
+    , emptySection
+    , emptyStep
+    , food2mL
+    , unitTomL
+    )
 
 
-type alias Lists =
-    { ingredients : List ( String, List String )
-    , kitchenware : List String
-    , units : List String
+type alias Recipe =
+    { title : String
+    , sections : List Section
     }
+
+
+emptyRecipe : Recipe
+emptyRecipe =
+    Recipe "" []
+
+
+type alias Section =
+    { title : String
+    , ingredients : List Ingredient
+    , steps : List Step
+    }
+
+
+emptySection : Section
+emptySection =
+    Section "" [] []
 
 
 type alias Ingredient =
     { name : String
-    , quantity : String
-    , unit : String
+    , kind : Kind
+    , quantity : Float
+    , unit : Unit
     }
 
 
-emptyLists : Lists
-emptyLists =
-    { ingredients = []
-    , kitchenware = []
-    , units = []
+emptyIngredient : Ingredient
+emptyIngredient =
+    { name = ""
+    , kind = Piece
+    , quantity = 1
+    , unit = Unit
     }
+
+
+type alias Step =
+    String
+
+
+emptyStep : Step
+emptyStep =
+    ""
+
+
+type Kind
+    = Flour
+    | GroundAlmonds
+    | Cocoa
+    | OatMeal
+    | WheatSeed
+    | Bicarbonate
+    | EggWhite
+    | EggYellow
+    | Jam
+    | BrownSugar
+    | IcingSugar
+    | Sugar
+    | Rice
+    | Butter
+    | Salt
+    | GrattedCheese
+    | Liquid
+    | Piece
+    | Cilentro
+    | Curry
+    | Curcuma
+    | InstantCoffee
+    | GroundCoffee
+    | Cinnamon
+    | Breadcrumbs
+    | Oil
+    | Honey
+    | Mustard
+    | Spread
+    | Parmesan
+    | PineNut
+    | Polenta
+    | Pepper
+    | Quinoa
+    | TomatoSauce
+
+
+type Unit
+    = Grams
+    | Centiliters
+    | Milliliters
+    | Cup -- Chef -- 250 mL
+    | HalfCup -- Cochon -- 125 mL
+    | ThirdCup -- Chat -- 80 mL
+    | QuarterCup -- Souris -- 60 mL
+    | Tablespoon -- Poule -- 15 mL
+    | Teaspoon -- Poussin -- 5 mL
+    | Oz -- 28g / 30mL
+    | Unit
+
+
+food2mL : Kind -> Maybe Float
+food2mL kind =
+    case kind of
+        Flour ->
+            Just (180 / 250)
+
+        PineNut ->
+            Just 0.55
+
+        Polenta ->
+            Just 0.75
+
+        Cocoa ->
+            Just 0.44
+
+        Breadcrumbs ->
+            Just 0.45
+
+        Cinnamon ->
+            Just 0.5
+
+        InstantCoffee ->
+            Just 0.2
+
+        GroundCoffee ->
+            Just 0.4
+
+        Cilentro ->
+            Just 0.068
+
+        Curry ->
+            Just 0.57
+
+        Curcuma ->
+            Just 0.59
+
+        WheatSeed ->
+            Just 0.75
+
+        GroundAlmonds ->
+            Just 0.36
+
+        OatMeal ->
+            Just 0.34
+
+        Bicarbonate ->
+            Just 0.87
+
+        EggWhite ->
+            Just 1.03
+
+        EggYellow ->
+            Just 1.13
+
+        Sugar ->
+            Just (230 / 250)
+
+        BrownSugar ->
+            Just 0.61
+
+        IcingSugar ->
+            Just 0.5
+
+        Rice ->
+            Just (200 / 250)
+
+        Butter ->
+            Just (220 / 250)
+
+        Salt ->
+            Just (340 / 250)
+
+        GrattedCheese ->
+            Just (100 / 250)
+
+        Liquid ->
+            Just 1
+
+        Jam ->
+            Just 1.37
+
+        Oil ->
+            Just 0.9
+
+        Honey ->
+            Just 1.4
+
+        Mustard ->
+            Just 1.05
+
+        Spread ->
+            Just 1.05
+
+        Parmesan ->
+            Just 0.42
+
+        Pepper ->
+            Just 0.5
+
+        Quinoa ->
+            Just 0.71
+
+        TomatoSauce ->
+            Just 0.91
+
+        Piece ->
+            Nothing
+
+
+unitTomL : Unit -> Maybe Float
+unitTomL unit =
+    case unit of
+        Grams ->
+            Nothing
+
+        Centiliters ->
+            Just 10
+
+        Milliliters ->
+            Just 1
+
+        Cup ->
+            Just 250
+
+        HalfCup ->
+            Just 125
+
+        ThirdCup ->
+            Just 80
+
+        QuarterCup ->
+            Just 60
+
+        Tablespoon ->
+            Just 15
+
+        Teaspoon ->
+            Just 5
+
+        Oz ->
+            Just 30
+
+        Unit ->
+            Nothing
--- src/Main.elm
+++ src/Main.elm
@@ -5,7 +5,6 @@ import Data.Kiddoz exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Http
-import Request.Lists exposing (decodeLists, getLists)
 import Yaml.Decode
 
 
@@ -14,51 +13,88 @@ type alias Flags =
 
 
 type alias Model =
-    { ingredients : List Ingredient
-    , lists : Lists
+    { recipe : Recipe
     }
 
 
 type Msg
-    = FetchedLists (Result Http.Error String)
+    = NoOp
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( { ingredients = [], lists = emptyLists }, getLists FetchedLists )
+    ( { recipe =
+            { title = "Tarte aux citrons"
+            , sections =
+                [ { title = "Pâte"
+                  , ingredients =
+                        [ Ingredient "Farine" Flour 250 Grams
+                        , Ingredient "Sucre" Sugar 100 Grams
+                        , Ingredient "Beurre" Butter 150 Grams
+                        , Ingredient "Œuf" Piece 1 Unit
+                        , Ingredient "Poudre d'amande" GroundAlmonds 100 Grams
+                        ]
+                  , steps =
+                        [ "Préchauffer le four à 180 °C (th. 6). "
+                        , "Dans un bol, mélanger le beurre avec le sucre glace"
+                        , "Ajouter l'oeuf puis la farine et la poudre d'amande (ne pas trop travailler la pâte)"
+                        , "Faire une boule avec la pâte, la mettre dans un film alimentaire et la laisser reposer au moins 15 minutes au frais."
+                        , "A l'aide d'un rouleau à pâtisserie, étaler la pâte, et la répartir ensuite dans un moule à tarte."
+                        ]
+                  }
+                , { title = "Appareil"
+                  , ingredients =
+                        [ Ingredient "Citrons" Piece 2 Unit
+                        , Ingredient "Sucre" Sugar 150 Grams
+                        , Ingredient "Œufs" Piece 3 Unit
+                        , Ingredient "Beurre" Butter 120 Grams
+                        ]
+                  , steps =
+                        [ "Dans une casserole, presser le jus des citrons et le mélanger avec le beurre."
+                        , "Les faire fondre à feu doux."
+                        , "Dans un bol, blanchir les oeufs avec le sucre en fouettant vigoureusement"
+                        , "Verser le beurre fondu et le jus de citron dessus."
+                        , "Verser l'appareil sur la pâte cuite et enfourner 20 à 30 minutes."
+                        ]
+                  }
+                ]
+            }
+      }
+    , Cmd.none
+    )
 
 
 view model =
     div [ id "content" ]
         [ img [ src "assets/images/Logo-KIDDOZ-01.svg", alt "Kiddoz logo", class "logo" ] []
         , h2 [] [ text "Convertisseur de recettes" ]
-        , Html.form [] []
+        , Html.form []
+            [ table []
+                [ thead []
+                    [ tr []
+                        [ th [] [ text "Ingrédient" ]
+                        , th [] [ text "Quantité" ]
+                        , th [] [ text "Unité" ]
+                        , th [] [ text "Kiddoz" ]
+                        ]
+                    ]
+                , tbody []
+                    [ tr []
+                        [ td [] [ input [ type_ "text", placeholder "Ingrédient" ] [] ]
+                        , td [] [ input [ type_ "text", placeholder "Quantité" ] [] ]
+                        , td [] [ input [ type_ "text", placeholder "Unité" ] [] ]
+                        , td [] []
+                        ]
+                    ]
+                ]
+            , div [] [ button [] [ text "Valider la recette " ] ]
+            ]
         ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        FetchedLists (Ok response) ->
-            case Yaml.Decode.fromString decodeLists response of
-                Ok lists ->
-                    ( { model | lists = lists }, Cmd.none )
-
-                Err error ->
-                    let
-                        _ =
-                            error
-                                |> Debug.log "Yaml Lists Decode Error"
-                    in
-                    ( model, Cmd.none )
-
-        FetchedLists (Err error) ->
-            let
-                _ =
-                    error
-                        |> Debug.log "Lists fetching failed"
-            in
-            ( model, Cmd.none )
+    ( model, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
