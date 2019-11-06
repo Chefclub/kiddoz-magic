bde2d5ace7b211e01a0bf5ba50126186214d2bd1
Rémy HUBSCHER <remy@chefclub.tv>
2019-11-06 13:27:20 +0100

    Add German translation.

--- src/L10n.elm
+++ src/L10n.elm
@@ -29,6 +29,162 @@ translateDE key =
         "Convertisseur de recettes" ->
             "Rezept-Konverter"
 
+        "Ingrédient" ->
+            "Zutat"
+
+        "Quantité" ->
+            "Menge"
+
+        "Unité" ->
+            "Einheit"
+
+        "Convertir" ->
+            "Konvertieren"
+
+        "Recommencer" ->
+            "Neu"
+
+        " de " ->
+            " "
+
+        "Ta recette" ->
+            "Dein Rezept"
+
+        "Ta recette Kiddoz" ->
+            "Dein Kiddoz Rezept"
+
+        "grams" ->
+            "Gramm"
+
+        "centiliters" ->
+            "Zentiliter"
+
+        "milliliters" ->
+            "Mililiter"
+
+        "cup" ->
+            "Cup"
+
+        "½ cup" ->
+            "½ Cup"
+
+        "⅓ cup" ->
+            "⅓ Cup"
+
+        "¼ cup" ->
+            "¼ Cup"
+
+        "tbsp" ->
+            "EL"
+
+        "tsp" ->
+            "TL"
+
+        "oz" ->
+            "oz"
+
+        "piece" ->
+            "Einheit"
+
+        "Farine" ->
+            "Mehl"
+
+        "Pignons" ->
+            "Pinienkerne"
+
+        "Polenta" ->
+            "Polenta"
+
+        "Chocolat" ->
+            "Schokolade"
+
+        "Miettes de pain" ->
+            "Brotkrümmel"
+
+        "Cannelle" ->
+            "Zimt"
+
+        "Café instantané" ->
+            "Instant Kaffee"
+
+        "Café moulu" ->
+            "Gemahlener Kaffee"
+
+        "Coriandre" ->
+            "Koriander"
+
+        "Curry" ->
+            "Curry"
+
+        "Curcuma" ->
+            "Kurkuma"
+
+        "Graines de blé" ->
+            "Weizenkörner"
+
+        "Amandes moulues" ->
+            "Gemahlene Mandeln"
+
+        "Flocons d'avoine" ->
+            "Haferflocken"
+
+        "Bicarbonate" ->
+            "Backpulver"
+
+        "Blancs d'œufs" ->
+            "Eiweiß"
+
+        "Jaunes d'œufs" ->
+            "Eigelb"
+
+        "Sucre en poudre" ->
+            "Zucker"
+
+        "Cassonade" ->
+            "Brauner Zucker"
+
+        "Sucre glace" ->
+            "Puderzucker"
+
+        "Riz" ->
+            "Reis"
+
+        "Beurre" ->
+            "Butter"
+
+        "Sel" ->
+            "Salz"
+
+        "Fromage râpé" ->
+            "Geriebener Käse"
+
+        "Liquides" ->
+            "Flüssigkeit"
+
+        "Confiture" ->
+            "Marmelade"
+
+        "Miel" ->
+            "Honig"
+
+        "Moutarde" ->
+            "Senf"
+
+        "Pâte à tartiner" ->
+            "Aufstrich"
+
+        "Parmesan" ->
+            "Parmesan"
+
+        "Poivre" ->
+            "Pfeffer"
+
+        "Quinoa" ->
+            "Quinoa"
+
+        "Sauce tomate" ->
+            "Tomatensoße"
+
         _ ->
             key
 
@@ -57,145 +213,143 @@ translateEN key =
         " de " ->
             " of "
 
-        "Farine" ->
-            "Flour"
-
         "Ta recette" ->
             "Your recipe"
 
         "Ta recette Kiddoz" ->
             "Your Kiddoz recipe"
 
-        --         "grams" ->
-        --             key
-        --
-        --         "centiliters" ->
-        --             key
-        --
-        --         "milliliters" ->
-        --             key
-        --
-        --         "cup" ->
-        --             key
-        --
-        --         "½ cup" ->
-        --             key
-        --
-        --         "⅓ cup" ->
-        --             key
-        --
-        --         "¼ cup" ->
-        --             key
-        --
-        --         "tbsp" ->
-        --             key
-        --
-        --         "tsp" ->
-        --             key
-        --
-        --         "oz" ->
-        --             key
-        --
-        --         "piece" ->
-        --             key
-        --
-        --         "Farine" ->
-        --             key
-        --
-        --         "Pignons" ->
-        --             key
-        --
-        --         "Polenta" ->
-        --             key
-        --
-        --         "Chocolat" ->
-        --             key
-        --
-        --         "Miettes de pain" ->
-        --             key
-        --
-        --         "Cannelle" ->
-        --             key
-        --
-        --         "Café instantané" ->
-        --             key
-        --
-        --         "Café moulu" ->
-        --             key
-        --
-        --         "Coriandre" ->
-        --             key
-        --
-        --         "Curry" ->
-        --             key
-        --
-        --         "Curcuma" ->
-        --             key
-        --
-        --         "Graines de blé" ->
-        --             key
-        --
-        --         "Amandes moulues" ->
-        --             key
-        --
-        --         "Flocons d'avoine" ->
-        --             key
-        --
-        --         "Bicarbonate" ->
-        --             key
-        --
-        --         "Blancs d'œufs" ->
-        --             key
-        --
-        --         "Jaunes d'œufs" ->
-        --             key
-        --
-        --         "Sucre en poudre" ->
-        --             key
-        --
-        --         "Cassonade" ->
-        --             key
-        --
-        --         "Sucre glace" ->
-        --             key
-        --
-        --         "Riz" ->
-        --             key
-        --
-        --         "Beurre" ->
-        --             key
-        --
-        --         "Sel" ->
-        --             key
-        --
-        --         "Fromage râpé" ->
-        --             key
-        --
-        --         "Liquides" ->
-        --             key
-        --
-        --         "Confiture" ->
-        --             key
-        --
-        --         "Miel" ->
-        --             key
-        --
-        --         "Moutarde" ->
-        --             key
-        --
-        --         "Pâte à tartiner" ->
-        --             key
-        --
-        --         "Parmesan" ->
-        --             key
-        --
-        --         "Poivre" ->
-        --             key
-        --
-        --         "Quinoa" ->
-        --             key
-        --
-        --         "Sauce tomate" ->
-        --             key
+        "grams" ->
+            "grams"
+
+        "centiliters" ->
+            "centiliters"
+
+        "milliliters" ->
+            "mililiters"
+
+        "cup" ->
+            "cup"
+
+        "½ cup" ->
+            "½ cup"
+
+        "⅓ cup" ->
+            "⅓ cup"
+
+        "¼ cup" ->
+            "¼ cup"
+
+        "tbsp" ->
+            "tbsp"
+
+        "tsp" ->
+            "tsp"
+
+        "oz" ->
+            "oz"
+
+        "piece" ->
+            "piece"
+
+        "Farine" ->
+            "Floor"
+
+        "Pignons" ->
+            "Pinenut"
+
+        "Polenta" ->
+            "Polenta"
+
+        "Chocolat" ->
+            "Cocoa"
+
+        "Miettes de pain" ->
+            "Breadcrumbs"
+
+        "Cannelle" ->
+            "Cinnamon"
+
+        "Café instantané" ->
+            "Instant coffee"
+
+        "Café moulu" ->
+            "Ground coffee"
+
+        "Coriandre" ->
+            "Cilendro"
+
+        "Curry" ->
+            "Curry"
+
+        "Curcuma" ->
+            "Curcuma"
+
+        "Graines de blé" ->
+            "Wheat seed"
+
+        "Amandes moulues" ->
+            "Ground almonds"
+
+        "Flocons d'avoine" ->
+            "Oat meal"
+
+        "Bicarbonate" ->
+            "Bicarbonate"
+
+        "Blancs d'œufs" ->
+            "Egg white"
+
+        "Jaunes d'œufs" ->
+            "Egg yellow"
+
+        "Sucre en poudre" ->
+            "Sugar"
+
+        "Cassonade" ->
+            "Brown sugar"
+
+        "Sucre glace" ->
+            "Icing sugar"
+
+        "Riz" ->
+            "Rice"
+
+        "Beurre" ->
+            "Butter"
+
+        "Sel" ->
+            "Salt"
+
+        "Fromage râpé" ->
+            "Gratted cheese"
+
+        "Liquides" ->
+            "Liquids"
+
+        "Confiture" ->
+            "Jam"
+
+        "Miel" ->
+            "Honey"
+
+        "Moutarde" ->
+            "Mustard"
+
+        "Pâte à tartiner" ->
+            "Spread"
+
+        "Parmesan" ->
+            "Parmesan"
+
+        "Poivre" ->
+            "Pepper"
+
+        "Quinoa" ->
+            "Quinoa"
+
+        "Sauce tomate" ->
+            "Tomato sauce"
+
         _ ->
             key
